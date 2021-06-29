d545ad6802cefaa25563f9fb25cbbdf4ddd66353
Marcio Fernandes <marciofso@hotmail.com>
2021-06-29 00:29:42 -0300

    creation of the PersonResource class and mapping

---
+++ src/main/java/com/marcio/personapi/resources/PersonResource.java
@@ -0,0 +1,10 @@
+package com.marcio.personapi.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/persons")
+public class PersonResource {
+
+}
