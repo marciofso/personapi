f88ef560c26bd794a10f2aaf0b047ebafe30b35a
Marcio Fernandes <marciofso@hotmail.com>
2021-06-29 12:01:55 -0300

    creation of the Phone class

---
+++ src/main/java/com/marcio/personapi/domain/Phone.java
@@ -0,0 +1,90 @@
+package com.marcio.personapi.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.marcio.personapi.enums.PhoneType;
+
+@Entity
+public class Phone implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private PhoneType type;
+
+	@Column(nullable = false, unique = true)
+	private String number;
+
+	public Phone() {
+
+	}
+
+	public Phone(Integer id, PhoneType type, String number) {
+		super();
+		this.id = id;
+		this.type = type;
+		this.number = number;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public PhoneType getType() {
+		return type;
+	}
+
+	public void setType(PhoneType type) {
+		this.type = type;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Phone other = (Phone) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+}
